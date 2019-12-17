193701b3316c89b2374c6ee60d08b494c9f1f906
Sertalp B. Cay <sertalp.cay@sas.com>
2019-12-17 15:34:49 -0500

    Script changes

--- generate_latest.sh
+++ generate_latest.sh
@@ -4,16 +4,20 @@ set -e
 (
     cd package/doc
     echo "*** Generating docs @ develop ***"
-    . makedocs.sh
+    #. makedocs.sh
 )
 
 (
-    echo "*** Copying latest version ***"
-    git -C gh-pages/sasoptpy rm -rf .
-    git -C gh-pages/sasoptpy clean -dfx
-    rsync -vazC package/doc/_build/html/ gh-pages/sasoptpy/
+    echo "*** Cleaning repository ***"
+    cd gh-pages/sasoptpy
+    git reset --hard
+    git checkout gh-pages
+    git clean -dfx
+    git rm -rf .
 )
 
+rsync -vazC package/doc/_build/html/ gh-pages/sasoptpy/
+
 echo "*** Adding old versions ***"
-. doc_generator.sh
+bash doc_generator.sh > doc_gen.log
 
