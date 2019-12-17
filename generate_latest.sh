089f35adc81ccd24e0e0696105a8472cfad58bf7
Sertalp B. Cay <sertalp.cay@sas.com>
2019-12-17 14:56:19 -0500

    Created subsessions

--- generate_latest.sh
+++ generate_latest.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 set -e
 
-cd package/doc
-echo "*** Generating docs @ develop ***"
-. makedocs.sh
+(
+    cd package/doc
+    echo "*** Generating docs @ develop ***"
+    . makedocs.sh
+)
 
-cd ..
-cd ..
-echo "*** Copying latest version ***"
-git -C gh-pages/sasoptpy rm -rf .
-git -C gh-pages/sasoptpy clean -dfx
-rsync -vazC package/doc/_build/html/ gh-pages/sasoptpy/
+(
+    echo "*** Copying latest version ***"
+    git -C gh-pages/sasoptpy rm -rf .
+    git -C gh-pages/sasoptpy clean -dfx
+    rsync -vazC package/doc/_build/html/ gh-pages/sasoptpy/
+)
 
 echo "*** Adding old versions ***"
 . doc_generator.sh
